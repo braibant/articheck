8e33149a0973a6b229729572a0a555e52af4418a
Thomas Braibant <thomas.braibant@inria.fr>
2014-02-06 16:05:46 +0100

    Proto

--- src/arti.ml
+++ src/arti.ml
@@ -1,65 +1,87 @@
-module PSet = struct
-  type 'a t = 
+module Ty = struct
+    
+  type 'a t =
     {
       eq: 'a -> 'a -> bool;
-      enum : 'a list
-    } 
-
+      mutable enum : 'a list;
+      fresh : ('a list -> 'a) option; 
+      uid : int;
+    }
+      
   let mem x s =
     List.exists (fun y -> y = x) s.enum
 
   let add x s = 
-    if mem x s then s else {s with enum = x::s.enum}
+    if mem x s then () else s.enum <- x::s.enum
 
   let elements s = s.enum
 
 end
-    
-type 'a ty =
-  {
-    mutable set : 'a PSet.t;
-    uid : int;
-  }
+
+type 'a ty = 'a Ty.t    
 
 type (_,_) fn = 
-| Return : 'a ty -> ('a,'a) fn
+| Constant : 'a ty -> ('a,'a) fn
 | Fun    : 'a ty * ('b, 'c) fn -> ('a -> 'b, 'c) fn;;
 
 let rec eval : type a b. (a,b) fn -> a -> b list = 
+		 let open Ty in 
   fun fd f ->
     match fd with
-    | Return ty -> [f]
+    | Constant ty -> [f]
     | Fun (ty,fd) -> 
-      List.flatten (List.map (fun e -> eval fd (f e)) (PSet.elements ty.set))
+      List.flatten (List.map (fun e -> eval fd (f e)) (ty.enum))
 ;;  
 
 let rec codom : type a b. (a,b) fn -> b ty = 
 		  function Fun (_,fd) -> codom fd
-		  | Return ty -> ty
+		  | Constant ty -> ty
 
 let use fd f = 
   let prod = eval fd f in 
   let ty = codom fd in 
-  ty.set <- List.fold_right (PSet.add) prod ty.set;
+  List.iter (fun x -> Ty.add x ty) prod;
   ()
 
+let declare_type () =
+  let id = ref 0 in 
+  fun ?fresh eq -> 
+    incr id; Ty.({ eq ; fresh; enum = []; uid = !id})
+
+let populate n ty = 
+  let open Ty in 
+  match ty.fresh with
+  | None -> invalid_arg "populate"
+  | Some fresh -> 
+  for i = 0 to n - 1 do
+    ty.enum <- fresh (ty.enum) :: ty.enum
+  done
+;;
+
+let (@->) ty fd = Fun (ty,fd)
+let returning ty = Constant ty
+
 module Sig = struct
-  type dyn = Dyn : ('a,'b) fn * 'a -> dyn
+  type elem = Elem : ('a,'b) fn * 'a -> elem
     
-  type t = (string * dyn) list 
+  type t = (string * elem) list 
+
+  let empty = []
 
   let add s id fd f =
-    (id, Dyn (fd,f))::s
+    (id, Elem (fd,f))::s
 
   type iteratee = {inhab : 'a 'b. string -> ('a,'b) fn -> 'a -> unit } 
 
   let iter (g:iteratee ) s =
     List.iter (fun (id,d) ->
       match d with
-	Dyn (fd,f) -> g.inhab id fd f
+	Elem (fd,f) -> g.inhab id fd f
       
     ) s
-      
+
+  let for_all ty f =
+    List.for_all f ty.Ty.enum
 end
 
 let rec ncheck n (s: Sig.t) = 
@@ -72,8 +94,33 @@ let rec ncheck n (s: Sig.t) =
     end
 
 
-  
-  
+(* Example *)
+module SIList = struct
+
+  type t = int list
+
+  let empty = []
+
+  let rec add x = function 
+    | [] -> [x]
+    | t::q -> if t < x then t :: add x q else x::t::q
+
+end
+ 
+
+let si_t : SIList.t ty = declare_type () (=)  
+let int_t : int ty = declare_type () ~fresh:(fun _ -> Random.int 1000)(=)
+let _ = populate 10 int_t
+
+
+let s = Sig.empty 
+let s = Sig.add s "empty" (returning si_t) SIList.empty
+let s = Sig.add s "add" (int_t @-> si_t @-> returning si_t) SIList.add
+
+let _ =  ncheck  5 s;;
+
+let _ = Sig.prop si_t (fun s -> List.sort Pervasives.compare s = s);;  
+
 
 
 
